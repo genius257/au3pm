e79006455638e898f3eb473d5fb6e50ca7ff0071
Anders Pedersen <hot415@hotmail.com>
2019-11-27 01:38:18 +0100

    Fixed issue on fresh clone the missing folder would result in an error from Aut2exe.exe

--- au3pm.bat
+++ au3pm.bat
@@ -10,6 +10,8 @@ IF BitVersion == 32 (set AutoItReg="HKEY_LOCAL_MACHINE\Software\AutoIt v3") ELSE
 
 FOR /F "skip=2 tokens=2,*" %%A IN ('reg query "%AutoItReg:"=%\AutoIt" /v "InstallDir"') DO set "AutoItDir=%%B"
 
+IF NOT EXIST "%~dp0\build\" (MD "%~dp0\build\")
+
 IF EXIST "%~dp0\build\au3pm.exe" (DEL "%~dp0\build\au3pm.exe")
 
 "%AutoItDir%\Aut2Exe\Aut2exe.exe" /in "%~dp0\au3pm.au3" /out "%~dp0\build\au3pm.exe" /x86 /console
